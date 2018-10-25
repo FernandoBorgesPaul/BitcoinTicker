393a1b4f0a113430cce98a14084edfd6a9214eee
Fernando Borges Paul <fernandoborgespaul@MacBook-Pro-de-Fernando.local>
2018-10-25 12:09:53 -0500

    Se actualiza el API URL en ViewController

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -1,14 +1,24 @@
-//
-//  ViewController.swift
-//  BitcoinTicker
-//
-//  Created by Angela Yu on 23/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        
+    }
     
     //MARK: - Protocolos de composición del UIPicker
     
@@ -27,36 +37,12 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         return currencyArray[row]
     }
     
+    //Determinamos que va a hacer el UIPicker con cada uno de los componentes de las filas al seleccionar cada una.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        print(finalURL)
     }
-    
-    
-    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-    var finalURL = ""
-
-    //Pre-setup IBOutlets
-    @IBOutlet weak var bitcoinPriceLabel: UILabel!
-    @IBOutlet weak var currencyPicker: UIPickerView!
-    
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-        currencyPicker.delegate = self
-        currencyPicker.dataSource = self
-        
-    }
-
-    
-    //TODO: Place your 3 UIPickerView delegate methods here
-    
-    
-    
-
-    
     
     
 //    
